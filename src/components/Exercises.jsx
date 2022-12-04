ae4fa0158ebd85f0308b17f688b5709b8f9d2dce
Joseph Huntley <Joseph.Huntley@outlook.com>
2022-12-04 15:05:55 -0500

    Update

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,7 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import Pagination from '@mui/material/Pagination';
+import { Box, Stack, Typography } from '@mui/material';
+import ExerciseCard from './ExerciseCard';
 
-const Exercises = () => {
-  return <div>Exercises</div>;
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
+const Exercises = ({ setExercises, bodyPart, exercises }) => {
+	const [currentPage, setCurrentPage] = useState(1);
+	const exercisePerPage = 9;
+
+	const paginate = (e, value) => {
+		setCurrentPage(value);
+
+		window.scrollTo({ top: '1800', behavior: 'smooth' });
+	};
+
+	useEffect(() => {
+		const fetchExercisesData = async () => {
+			let exercisesData = [];
+			if (bodyPart.data === 'all') {
+				exercisesData = await fetchData(
+					'https://exercisedb.p.rapidapi.com/exercises',
+					exerciseOptions
+				);
+			} else {
+				exercisesData = await fetchData(
+					`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+					exerciseOptions
+				);
+			}
+
+			setExercises(exercisesData);
+		};
+		fetchExercisesData();
+	}, [bodyPart]);
+
+	const indexOfLastExercise = currentPage * exercisePerPage;
+	const indexOfFirstExercise = indexOfLastExercise - exercisePerPage;
+	const currentExercises = exercises.slice(
+		indexOfFirstExercise,
+		indexOfLastExercise
+	);
+
+	return (
+		<Box
+			id='exercises'
+			sx={{ mt: { lg: '110px' } }}
+			mt='50px'
+			p='20px'>
+			<Typography
+				variant='h3'
+				mb='46px'>
+				Showing Result
+			</Typography>
+			<Stack
+				direction='row'
+				sx={{ gap: { lg: '110px', xs: '50px' } }}
+				flexWrap='wrap'
+				justifyContent='center'>
+				{currentExercises.map((exercise, index) => (
+					<ExerciseCard
+						key={index}
+						exercise={exercise}
+					/>
+				))}
+			</Stack>
+			<Stack
+				mt='100px'
+				alignItems='center'>
+				{exercises.length > exercisePerPage && (
+					<Pagination
+						color='standard'
+						shap='rounded'
+						defaultPage={1}
+						count={Math.ceil(exercises.length / exercisePerPage)}
+						page={currentPage}
+						onChange={paginate}
+						size='large'
+					/>
+				)}
+			</Stack>
+		</Box>
+	);
 };
 
 export default Exercises;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,29 @@
-import React from "react";
-import { useState } from "react";
-import { Box } from "@mui/material";
-import Exercises from "../components/Exercises";
-import HeroBanner from "../components/HeroBanner";
-import SearchExercises from "../components/SearchExercises";
+import React from 'react';
+import { useState } from 'react';
+import { Box } from '@mui/material';
+import Exercises from '../components/Exercises';
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
-  const [exercises, setExercises] = useState([]);
-  const [bodyPart, setBodyPart] = useState("all");
-  return (
-    <Box>
-      <HeroBanner />
-      <SearchExercises
-        setExercises={setExercises}
-        bodyPart={bodyPart}
-        setBodyPart={setBodyPart}
-      />
-      <Exercises
-        setBodyPart={setBodyPart}
-        bodyPart={bodyPart}
-        setExercises={setExercises}
-      />
-    </Box>
-  );
+	const [exercises, setExercises] = useState([]);
+	const [bodyPart, setBodyPart] = useState('all');
+
+	return (
+		<Box>
+			<HeroBanner />
+			<SearchExercises
+				setExercises={setExercises}
+				bodyPart={bodyPart}
+				setBodyPart={setBodyPart}
+			/>
+			<Exercises
+				setExercises={setExercises}
+				exercises={exercises}
+				bodyPart={bodyPart}
+			/>
+		</Box>
+	);
 };
 
 export default Home;
